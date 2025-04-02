67399a4fff66a50e355ef298ee1111a0ba3677c1
Hernan Escobar <98986488+Hernan3scobar@users.noreply.github.com>
2025-04-02 13:33:50 -0300

    SCRM-59: Add EC2 instance with SSH/HTTP access and key pair (#6)

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "my_key_pair"
+  public_key = file("~/.ssh/my_shh_key.pub")
+}
+
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.ssh_key.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "web-instance"
+  }
+}
