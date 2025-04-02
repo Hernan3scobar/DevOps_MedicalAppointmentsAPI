86a9634169a4adb64dbd3e4ade6b3fe4dd43b2ac
Hernan Escobar <98986488+Hernan3scobar@users.noreply.github.com>
2025-04-02 13:40:41 -0300

    SCRM-60: Add PostgreSQL RDS instance with subnet group and secure SG for EC2 access (#7)

---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,41 @@
+resource "aws_db_subnet_group" "rds" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = "RDS subnet group"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow DB connections"
+  vpc_id      = var.vpc_id
+
+ ingress {
+  from_port   = 5432
+  to_port     = 5432
+  protocol    = "tcp"
+  security_groups = [var.ec2_sg_id] 
+ }
+
+ egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "15.3"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+}
+
