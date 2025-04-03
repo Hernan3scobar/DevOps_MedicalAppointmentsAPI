cfa45c88e58145b138911f66a078f8c90e296758
Hernan Escobar <98986488+Hernan3scobar@users.noreply.github.com>
2025-04-03 17:21:40 -0300

    Scrm 70 fix issues detected during terraform apply in vpc and rds module (#13)
    * SCRM-73: Fix RDS, VPC apply errors and integrate SSM Parameter Store
    
    * Change RDS module to mysql

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -13,8 +13,8 @@ resource "aws_security_group" "rds_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 5432
-    to_port         = 5432
+    from_port       = 3306
+    to_port         = 3306
     protocol        = "tcp"
     security_groups = [var.ec2_sg_id]
   }
@@ -29,10 +29,10 @@ resource "aws_security_group" "rds_sg" {
 
 resource "aws_db_instance" "default" {
   allocated_storage      = 20
-  engine                 = "postgres"
-  engine_version         = "15.8"
+  engine                 = "mysql"
+  engine_version         = "8.0"
   instance_class         = "db.t3.micro"
-  username               = "postgres_user"
+  username               = "admin_user" 
   password               = data.aws_ssm_parameter.db_password.value
   db_subnet_group_name   = aws_db_subnet_group.rds.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
