db9046742a42f5c47bd6bda5bac97e03dd6fe0e9
Hernan Escobar <98986488+Hernan3scobar@users.noreply.github.com>
2025-04-04 01:13:23 -0300

    WIP: fixing pass promt (#16)

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -2,10 +2,6 @@ variable "vpc_id" {}
 variable "subnet_ids" {
   type = list(string)
 }
-variable "db_password" {
-  description = "Password for the RDS PostgreSql instance"
-  sensitive   = true
-}
 
 variable "ec2_sg_id" {
   description = "Security group ID of EC2 instance"
