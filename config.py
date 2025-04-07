26cb70224c09c0102150f9f3afce4d57c2e82c9e
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-04-06 22:22:18 -0400

    refactor: moving files to /app directory

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///schedule.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = os.environ.get('DEBUG', 'False') == 'True'  # Converts to boolean value
-    
--- landing.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp_landing = Blueprint('landing', __name__, template_folder='templates')
-
-@bp_landing.route('/api')
-def home():
-    return render_template('index.html')
