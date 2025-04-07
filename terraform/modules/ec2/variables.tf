6231ded8d8d69f2dd1cc9e9da892f3386b10fbc2
CodeSSRockMan <41550641+CodeSSRockMan@users.noreply.github.com>
2025-04-07 10:52:05 -0400

    moving variable to ec2 module

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,2 +1,7 @@
 variable "vpc_id" {}
 variable "subnet_id" {}
+variable "ssh_private_key" {
+  description = "Private SSH key for EC2 instances"
+  type        = string
+  default     = ""  # Puede estar vacío si no se quiere asignar un valor por defecto
+}
