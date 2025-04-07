442798c608f6c1cd1dd4d5529e66797da73ccc55
CodeSSRockMan <41550641+CodeSSRockMan@users.noreply.github.com>
2025-04-07 10:35:28 -0400

    provider fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   region                   = var.region
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "hernasoftserve"
+  profile                  = "Pedro_Softserve"
 }
 
 
