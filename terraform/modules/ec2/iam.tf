d80fba651966962f6fb4ca7ee3f2b0a0a1679f8f
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-04-06 22:25:35 -0400

    infra: add iam roles for budgets, jenkins and formating files

--- terraform/modules/ec2/iam.tf
+++ terraform/modules/ec2/iam.tf
@@ -1,13 +1,16 @@
+
+# This module creates an IAM role and policies for EC2 instances to access SSM parameters and manage budgets.
 resource "aws_iam_role" "ec2_ssm_role" {
   name = "EC2SSMRole"
+
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
           Service = "ec2.amazonaws.com"
-        }
+        },
         Action = "sts:AssumeRole"
       },
     ]
@@ -37,12 +40,7 @@ resource "aws_iam_policy" "ssm_read_policy" {
     ]
   })
 }
-
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ec2_ssm_role.name
-  policy_arn = aws_iam_policy.ssm_read_policy.arn
-}
-
+# Allow management of budgets in AWS
 resource "aws_iam_policy" "budget_policy" {
   name        = "AllowBudgetManagement"
   description = "Allow management of budgets in AWS"
@@ -64,15 +62,81 @@ resource "aws_iam_policy" "budget_policy" {
     ]
   })
 }
+# Allow Jenkins to manage EC2 instances
+resource "aws_iam_policy" "jenkins_permissions" {
+  name = "JenkinsInfrastructureAccess"
 
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
 
+      # RDS
+      {
+        Effect = "Allow",
+        Action = [
+          "rds:CreateDBInstance",
+          "rds:DeleteDBInstance",
+          "rds:DescribeDBInstances",
+          "rds:ModifyDBInstance"
+        ],
+        Resource = "*"
+      },
 
+      # EC2
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:RunInstances",
+          "ec2:DescribeInstances",
+          "ec2:TerminateInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          "ec2:CreateTags"
+        ],
+        Resource = "*"
+      },
+
+      # VPC
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:DescribeRouteTables",
+          "ec2:CreateRouteTable",
+          "ec2:AssociateRouteTable"
+        ],
+        Resource = "*"
+      },
+
+      # IAM: 
+      {
+        Effect   = "Allow",
+        Action   = "iam:PassRole",
+        Resource = "*"
+      }
+    ]
+  })
+}
+# Attach the policies to the EC2 role
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = aws_iam_policy.ssm_read_policy.arn
+}
+# Attach the policies to budget management role
 resource "aws_iam_role_policy_attachment" "budget_access" {
-  role       = aws_iam_role.ec2_ssm_role.name  
+  role       = aws_iam_role.ec2_ssm_role.name
   policy_arn = aws_iam_policy.budget_policy.arn
 }
-
-
+# Attach the policies to Jenkins role
+resource "aws_iam_role_policy_attachment" "jenkins_permissions_attach" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = aws_iam_policy.jenkins_permissions.arn
+}
+# Create an instance profile for the EC2 role
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "EC2SSMProfile"
   role = aws_iam_role.ec2_ssm_role.name
--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,38 +1,53 @@
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-sg"
-  description = "Allow SSH and HTTP"
+  description = "Security group for EC2 with separated rules"
   vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
+resource "aws_security_group_rule" "ssh_ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow SSH"
+}
+
+resource "aws_security_group_rule" "http_ingress" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow HTTP"
+}
+
+resource "aws_security_group_rule" "jenkins_ingress" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow Jenkins HTTP access"
+}
+
+resource "aws_security_group_rule" "all_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+  description       = "Allow all outbound traffic"
+}
 
 resource "aws_key_pair" "ssh_key" {
   key_name   = "my_key_pair"
   public_key = file("~/.ssh/my_shh_key.pub")
 }
-
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
@@ -46,3 +61,4 @@ resource "aws_instance" "web" {
     Name = "web-instance"
   }
 }
+
