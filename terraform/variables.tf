6cf7c7ffa046ac3064ea1a7fac53744f84801c76
CodeSSRockMan <41550641+CodeSSRockMan@users.noreply.github.com>
2025-04-06 21:49:49 -0400

    Scrm 74 separate tasks (#19)
    * update gitignore (#18)
    
    * terraform provisioning for jenkins
    
    * current jenkins pipeline for terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,25 @@
-variable "region" {}
-variable "vpc_cidr" {}
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
 
 variable "budget_name" {}
 variable "limit_amount" {}
 variable "services" {
-  type = list(string)
+  description = "AWS Services to monitor in the budget"
+  type        = list(string)
+  default     = ["AmazonEC2", "AmazonRDS", "AmazonVPC"] # Valores por defecto
 }
 variable "time_period_start" {}
 variable "time_period_end" {}
 variable "notification_email" {}
 variable "time_unit" {}
+variable "ssh_public_key" {
+  description = "Public SSH key for EC2 instances"
+  type        = string
+  default     = "" # Puede estar vacío por defecto
+}
