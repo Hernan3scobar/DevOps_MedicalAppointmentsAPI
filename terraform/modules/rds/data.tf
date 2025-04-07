d6a51a5ed0b8eb9c306e4877f59cc5f8330ceb0d
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-04-07 00:21:35 -0400

    infra: add the ssm paramter to the username for the db

--- terraform/modules/rds/data.tf
+++ terraform/modules/rds/data.tf
@@ -1,4 +1,8 @@
 data "aws_ssm_parameter" "db_password" {
-  name            = "/rds/postgres/password"
+  name            = "/rds/mysql/password"
   with_decryption = true
+}
+
+data "aws_ssm_parameter" "db_user" {
+  name = "/rds/mysql/username"
 }
