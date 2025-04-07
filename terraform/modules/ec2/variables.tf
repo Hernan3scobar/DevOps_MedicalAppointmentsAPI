15be6458f81f017700b3473c264f0327297940c5
CodeSSRockMan <41550641+CodeSSRockMan@users.noreply.github.com>
2025-04-07 10:47:20 -0400

    fix duplicate values

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,11 +1,2 @@
 variable "vpc_id" {}
 variable "subnet_id" {}
-variable "ssh_public_key" {
-  description = "Public SSH key for EC2 instances"
-  type        = string
-}
-variable "ssh_private_key" {
-  description = "Private SSH key for EC2 instances"
-  type        = string
-  default     = ""  # Puede estar vacío si no se quiere asignar un valor por defecto
-}
