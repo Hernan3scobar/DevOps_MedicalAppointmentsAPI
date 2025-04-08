1652ad2b628f24f2323ac8921d2aa89c1a0250ce
CodeSSRockMan <41550641+CodeSSRockMan@users.noreply.github.com>
2025-04-08 14:37:58 -0400

    typo fix

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -33,6 +33,6 @@ resource "aws_db_instance" "default" {
   db_subnet_group_name   = aws_db_subnet_group.rds.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot    = true
-  deletion_protetion     = true # Set to true to prevent accidental deletion
+  delection_protetion     = true # Set to true to prevent accidental deletion
 }
 
