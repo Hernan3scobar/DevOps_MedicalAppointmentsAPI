9da0f1666bfa26bde217a8b217b7327cd45e1879
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-04-09 16:08:44 -0400

    fixing typo

--- terraform/modules/rds/data.tf
+++ terraform/modules/rds/data.tf
@@ -2,7 +2,7 @@ data "aws_ssm_parameter" "db_user" {
   name = "/rds/mysql/username"
 }
 
-data "aws_ssm_parameter" "db_pass" {
+data "aws_ssm_parameter" "db_password" {
   name            = "/rds/mysql/password"
   with_decryption = true
 }
