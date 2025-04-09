49b696d24f624e2d0d111b3903eafb103e802cc8
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-04-09 15:55:33 -0400

    adding db_url

--- terraform/modules/rds/data.tf
+++ terraform/modules/rds/data.tf
@@ -1,9 +1,13 @@
-data "aws_ssm_parameter" "db_password" {
-  name            = "/rds/mysql/password"
-  with_decryption = true
-}
-
 data "aws_ssm_parameter" "db_user" {
   name = "/rds/mysql/username"
 }
 
+data "aws_ssm_parameter" "db_pass" {
+  name            = "/rds/mysql/password"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "db_url" {
+  name            = "/rds/mysql/db_url"
+  with_decryption = true
+}
