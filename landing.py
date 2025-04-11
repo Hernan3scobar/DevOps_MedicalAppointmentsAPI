b2559bd96baabc64441e59d3d0ad424eedd167ee
Hernan Escobar <98986488+Hernan3scobar@users.noreply.github.com>
2025-04-11 02:09:04 -0400

    Update landing.py
    ✅ API is running. Visit /api for the web view or /api/schedules for data."

--- landing.py
+++ landing.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 bp_landing = Blueprint('landing', __name__, template_folder='templates')
 
+@bp_landing.route('/')
+def landing():
+    return "✅ API is running. Visit /api for the web view or /api/schedules for data."
+
 @bp_landing.route('/api')
 def home():
     return render_template('index.html')
