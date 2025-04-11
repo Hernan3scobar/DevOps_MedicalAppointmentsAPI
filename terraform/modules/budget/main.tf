325afc67bf5871b9af145ac1a2c1574209910aab
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-04-10 23:19:40 -0400

    formating

--- terraform/modules/budget/main.tf
+++ terraform/modules/budget/main.tf
@@ -1,26 +1,26 @@
 resource "aws_budgets_budget" "daily_budget" {
-  name         = var.budget_name
-  budget_type  = "COST"
-  limit_amount = var.limit_amount
-  limit_unit   = "USD"
-  time_unit    = var.time_unit 
+  name              = var.budget_name
+  budget_type       = "COST"
+  limit_amount      = var.limit_amount
+  limit_unit        = "USD"
+  time_unit         = var.time_unit
   time_period_end   = var.time_period_end
-  time_period_start = var.time_period_start 
+  time_period_start = var.time_period_start
 
-  cost_filter  {
-    name = "Service"
+  cost_filter {
+    name   = "Service"
     values = var.services
   }
 
 
   notification {
-    comparison_operator = "GREATER_THAN"
-    notification_type   = "ACTUAL"
-    threshold           = 80
-    threshold_type      = "PERCENTAGE"
+    comparison_operator        = "GREATER_THAN"
+    notification_type          = "ACTUAL"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
     subscriber_email_addresses = [var.notification_email]
-  
-    
+
+
   }
 }
 
